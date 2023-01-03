134f1c845a9efa19e618b23dfb6cbf77a5560759
f.messaoudi <firas.messaoudi@sofrecom.com>
2023-01-03 10:28:50 +0100

    add elk and ingress

--- scripts/deploy-db.sh
+++ scripts/deploy-db.sh
@@ -2,10 +2,10 @@
 
 #set -x
 kubectl create serviceaccount api-service-account
-kubectl apply  -f ../rbac-cluster-role.yaml
+kubectl apply  -f ../k8s/rbac-cluster-role.yaml
 kubectl create clusterrolebinding service-pod-reader-default --clusterrole=microservices-kubernetes-namespace-reader --serviceaccount=default:api-service-account
 
-kubectl apply -f ../k8s/postgresK8S/database-operation-sercret.yaml
+kubectl apply -f ../k8s/postgresK8S/database-operation-secret.yaml
 kubectl apply -f ../k8s/postgresK8S/database-operation-storage.yaml
 kubectl apply -f ../k8s/postgresK8S/database-operation-deployment.yaml
 kubectl apply -f ../k8s/postgresK8S/database-operation-service.yaml
